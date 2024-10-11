c19f899f1907e2bf69a595149050867d4021b351
SaharBahri <emailsaharbahri@gmail.com>
2024-10-11 10:49:54 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SessionItemComponent } from './session-item/session-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SessionItemComponent
   ],
   imports: [
     BrowserModule,
